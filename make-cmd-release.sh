80b6c7dd48a3a11c6c52c99852bc02891b301ca9
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-06-18 22:15:57 +0400

    add make-cmd-release.sh

---
+++ make-cmd-release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eu
+THIS="$(realpath "$0")"
+THISDIR="$(dirname "${THIS}")"
+
+cd "$THISDIR"
+
+# Create a distributable archive of the current version of Makeself with Windows support.
+
+VER="$(cat VERSION)"
+mkdir -p /tmp/makeself-"$VER" release
+cp -pPR makeself* cmd-header.sh README.md COPYING VERSION /tmp/makeself-"$VER"/
+./cmd-header.sh
+./makeself.sh --header ./makeself-cmd-header.sh --notemp /tmp/makeself-"$VER" release/makeself-"$VER".run.bat "Makeself v$VER" echo "Makeself has extracted itself"
+
